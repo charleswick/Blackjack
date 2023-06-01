eb8bb3c72cfd002f240b9fd97a32309e3798698a
charleswick <charles_wick25@milton.edu>
2023-05-31 21:15:16 -0400

    finished card class

--- src/Card.java
+++ src/Card.java
@@ -23,6 +23,74 @@ public class Card {
 
         }
 
+        if(pName==0){
+            name = "Ace";
+            value=11;
+
+        }
+        if(pName==1){
+            name = "Two";
+            value=2;
+
+        }
+        if(pName==2){
+            name = "Three";
+            value=3;
+
+        }
+        if(pName==3){
+            name = "Four";
+            value=4;
+
+        }
+        if(pName==4){
+            name = "Five";
+            value=5;
+
+        }
+        if(pName==5){
+            name = "Six";
+            value=6;
+
+        }
+        if(pName==6){
+            name = "Seven";
+            value=7;
+
+        }
+        if(pName==7){
+            name = "Eight";
+            value=8;
+
+        }
+        if(pName==8){
+            name = "Nine";
+            value=9;
+
+        }
+        if(pName==9){
+            name = "Ten";
+            value=10;
+
+        }
+        if(pName==10){
+            name = "Jack";
+            value=10;
+
+        }
+        if(pName==11){
+            name = "Queen";
+            value=10;
+
+        }
+        if(pName==12){
+            name = "King";
+            value=10;
+        }
+        isDealt=false;
+
+
+
 
 
 
