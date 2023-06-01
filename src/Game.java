347e10f7302e74a45e0b76c13c596b6b89862bbe
charleswick <charles_wick25@milton.edu>
2023-06-01 09:55:39 -0400

    finished all classes, started Game.java

--- src/Game.java
+++ src/Game.java
@@ -1,8 +1,36 @@
 public class Game {
+    public Player Dealer;
+    public Player Player;
+
+
     public static void main(String[] args) {
         Game blackjack = new Game();
+        Player Dealer = new Player();
+        Player player = new Player();
+        Dealer.isDealer=true;
+
+
+
 
     }
+
+
+
+    public int deck[]= new int[52];
+
+    public void fillDeck(){
+        for (int i = 0; i<deck.length; i++){
+
+        }
+    }
+
+
+    public void printLength() {
+        // how many total plants are there in the row?
+        System.out.println(deck.length);
+
+    }
+
 
 
 
--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,21 @@
+import java.util.ArrayList;
+
 public class Player {
+    public int currentValue;
+    public Boolean isBust;
+    public Boolean isDealer;
+    public ArrayList<Card> hand = new ArrayList<Card>();
+    public Player() {
+        for (Card b: hand){
+            currentValue+= b.value;
+        }
+
+        isBust=false;
+        isDealer=false;
+        if (currentValue>21){
+            isBust=true;
+        }
+
+
+    }
 }
